f125c0196d1978684e2942ce2f45a57a909e56d4
angelica zanibellato <62056718+angelicazanibellato@users.noreply.github.com>
2020-04-01 22:29:58 +0200

    Add files via upload

---
+++ angelicazanibellato/Esercizi p5/scontri tra camminatori/sketch.js
@@ -0,0 +1,47 @@
+let position;
+let velocity;
+
+let camminatore = [];
+
+function setup() {
+  createCanvas(600, 600);
+  for (let i=0; i< 50; i++) {
+  camminatore[i] = new Walker();  
+  }
+}
+
+function draw() {
+  background(16, 33, 232);
+  
+  for (let i=0; i<camminatore.length; i++) {
+ 
+    camminatore[i].step();
+    camminatore[i].display();
+  }
+}
+
+  class Walker {
+    constructor() {  //funzione setup che definisce la posizione iniziale
+  this.position = createVector(random(10),random(10));
+  this.velocity = createVector(random(1), random(2.3));
+    }
+    
+    display() {
+    fill (255, 204, 0);
+    stroke(255);
+    ellipse (this.position.x, this.position.y, 50, 50);
+    }
+    
+    step() {
+      this.position.add(this.velocity);
+      
+    if ((this.position.x > width) || (this.position.x < 0)) {
+      this.velocity.x = this.velocity.x * -1;
+    } 
+    
+    if ((this.position.y > height) || (this.position.y <0)) {
+      this.velocity.y = this.velocity.y * -1;
+
+  }
+ }
+ } 
