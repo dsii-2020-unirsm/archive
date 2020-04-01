a73765ca2dae3a8dd319fe98d29ea9b94e9a5642
Pietro B <61426274+peterbaru@users.noreply.github.com>
2020-04-01 23:11:20 +0200

    caricamento esercizi camminatori indipendenti

---
+++ peterbaru/Esercizi/camminatoriindipendenti/camminatoriindipendenti bolle/sketch.js
@@ -0,0 +1,56 @@
+let posizione;
+let velocita;
+let palline = 150;
+
+let camminatore = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  for (let i=0; i<palline; i++) {
+  camminatore[i] = new Walker();  
+  }
+}
+
+function draw() {
+  background(255);
+  
+  for (let i=0; i<camminatore.length; i++) {
+ 
+    camminatore[i].muoviti();
+    camminatore[i].mostrati();
+  }
+}
+
+  class Walker {
+    constructor() {  //funzione setup che definisce la posizione iniziale
+  this.posizione = createVector(random(width),random(height));
+  this.velocita = createVector(random(-1,1), random(-2));
+  this.colorer = random(255);
+  this.coloreg = random(0,50);
+  this.coloreb = random(255);    
+  this.dim = random(50);
+    }
+    
+    mostrati() {
+    fill (0, this.coloreg, this.coloreb, 50);
+    ellipse (this.posizione.x, this.posizione.y, this.dim, this.dim);
+    }
+    
+    muoviti() {
+      this.posizione.add(this.velocita);
+      
+    if (this.posizione.y > height) {
+      this.posizione.y = 0;
+    } else if (this.posizione.y < 0) {
+      this.posizione.y = height;
+    } 
+    
+    if ((this.posizione.x > width) || (this.posizione.x <0)) {
+      this.velocita.x = this.velocita.x * -1;
+
+   }
+  }
+ }
+
+
---
+++ peterbaru/Esercizi/camminatoriindipendenti/camminatoriindipendenti coriandoli/sketch.js
@@ -0,0 +1,53 @@
+let posizione;
+let velocita;
+let palline = 300;
+
+let camminatore = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  for (let i=0; i<palline; i++) {
+  camminatore[i] = new Walker();  
+  }
+}
+
+function draw() {
+  background(220);
+  
+  for (let i=0; i<camminatore.length; i++) {
+ 
+    camminatore[i].muoviti();
+    camminatore[i].mostrati();
+  }
+}
+
+  class Walker {
+    constructor() {  //funzione setup che definisce la posizione iniziale
+  this.posizione = createVector(random(width),random(height));
+  this.velocita = createVector(random(3), random(1.5));
+  this.colorer = random(255);
+  this.coloreg = random(255);
+  this.coloreb = random(255);    
+    }
+    
+    mostrati() {
+    fill (this.colorer, this.coloreg, this.coloreb);
+    ellipse (this.posizione.x, this.posizione.y, 30, 30);
+    }
+    
+    muoviti() {
+      this.posizione.add(this.velocita);
+      
+    if (this.posizione.x > width) {
+      this.posizione.x = 0;
+    } else if (this.posizione.x < 0) {
+      this.posizione.x = width;
+    } 
+    
+    if ((this.posizione.y > height) || (this.posizione.y <0)) {
+      this.velocita.y = this.velocita.y * -1;
+
+   }
+  }
+ } 
