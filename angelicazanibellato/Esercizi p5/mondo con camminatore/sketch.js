add1dbd96a2c5ff2294e5eab290f078b17f3d738
angelica zanibellato <62056718+angelicazanibellato@users.noreply.github.com>
2020-04-14 10:33:09 +0200

    Add files via upload

---
+++ angelicazanibellato/Esercizi p5/mondo con camminatore/sketch.js
@@ -0,0 +1,53 @@
+
+let camminatore = [];
+
+function setup() {
+  createCanvas(600,600);
+for (let i=0; i< 50; i++) {
+  camminatore[i] = new Walker();  
+  }
+  camminatore = new Walker();
+ background('#353FF2');
+
+
+}
+
+function draw() {
+  camminatore.step();
+  camminatore.render();
+ for (let i=0; i<camminatore.length; i++) {
+ 
+    camminatore[i].step();
+    camminatore[i].display();
+  }
+}
+
+
+class Walker {
+  constructor(){
+    //posizione di partenza
+    this.x = width/2;
+    this.y = height/2;
+  }
+
+  render() {
+    stroke('#F2D64B');
+    fill(255);
+    ellipse(this.x,this.y,25,25);
+  }
+
+  step() {
+    var choice = floor(random(4));
+    if (choice == 0) {
+      this.x++;
+    } else if (choice == 1) {
+      this.x--;
+    } else if (choice == 2) {
+      this.y++;
+    } else {
+      this.y--;
+    }
+    this.x = constrain(this.x,0,width-1);
+    this.y = constrain(this.y,0,height-1);
+  }
+}
