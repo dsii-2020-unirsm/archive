ab4c58a2b0c003b56834be8b1b6814bbf82efb8e
eleonoradfr <61805258+eleonoradfr@users.noreply.github.com>
2020-04-14 16:32:11 +0200

    Upload versione 1 generatore di pattern con modulo

---
+++ eleonoradfr/Esercitazioni_p5/2_generatore_pattern_modulo/versione_1/sketch.js
@@ -0,0 +1,24 @@
+function setup() { 
+  createCanvas(500, 500);
+  frameRate(10);
+  rectMode(CENTER);
+} 
+
+function draw() { 
+  background(10);
+  for (var i = 0  ;  i < 25  ;  i++) {
+    for (var v= 0; v < 25; v++) {
+    
+    if (i % 3 == 0 || v % 3 == 0) {
+      strokeWeight(2);
+      stroke(random(255), random(255), 255);
+      noFill();
+      ellipse(i*25, v*25, 10, 10);
+      
+    }else {
+      fill(random(255), random(255), 255);
+      rect(i*25, v*25, frameCount%20, frameCount%20);
+      }
+    }
+  }
+}
