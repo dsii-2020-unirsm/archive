5a3e4425b4d34f729260f844cf5292aa23808a0f
Pietro B <61426274+peterbaru@users.noreply.github.com>
2020-05-27 10:23:16 +0200

    caricamento commento

--- peterbaru/Esercizi/generatoreparole/sketch.js
+++ peterbaru/Esercizi/generatoreparole/sketch.js
@@ -1,5 +1,5 @@
 // -
-// Generatore casuale di parole 0.2 by Pietro [parole, random]
+// Generatore casuale di parole 0.2 by peterbaru [parole, random]
 // 2020 © Pietro, Daniele @Fupete and the course DSII2020 at DESIGN.unirsm 
 // github.com/dsii-2020-unirsm — github.com/fupete
 // Educational purposes, MIT License, 2020, San Marino
