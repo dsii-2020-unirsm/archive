4e61f7b783bbf22c0a8c560184c8164f7b90ed73
Pietro B <61426274+peterbaru@users.noreply.github.com>
2020-06-19 19:12:26 +0200

    caricamento file sketch.js aggiornato

--- peterbaru/MakingVisible/Prototipo/sketch.js
+++ peterbaru/MakingVisible/Prototipo/sketch.js
@@ -1,5 +1,5 @@
 // -
-// Stories.jpg 2.0 by peterbaru [racconti, disengi]
+// DrawnStories 2.0 by peterbaru [racconti, disengi]
 // 2020 © Pietro, Daniele @Fupete and the course DSII2020 at DESIGN.unirsm
 // github.com/dsii-2020-unirsm — github.com/fupete
 // Educational purposes, MIT License, 2020, San Marino
@@ -24,14 +24,24 @@
 //
 // —
 
-
 let button1;
 let button2;
+let button5;
 let centra = 45; //variabile per centrare i bottoni iniziali
 let xbottoni = 800; //variabile x della posizione bottoni
 let seleziona; //variabile per capire cosa si ha selezionato
 let lunghezza_racconto
 let temperatura_racconto
+
+let immagini_genitori = [];
+let immagini_totali;
+let rnd1;
+let rnd2;
+let rnd3;
+let rnd4;
+var cnv2;
+var canvasgenX = 640;
+var canvasgenY = 426;
 
 //variabili per CharRNN
 let charRNN;
@@ -57,46 +67,65 @@ let classifier1;
 function preload() {
   classifier = ml5.imageClassifier('DoodleNet');
   classifier1 = ml5.imageClassifier('MobileNet');
+
+  let img0 = loadImage('images/img/img0.png');
+  let img1 = loadImage('images/img/img1.png');
+  let img2 = loadImage('images/img/img2.png');
+  let img3 = loadImage('images/img/img3.png');
+  let img4 = loadImage('images/img/img4.png');
+  let img5 = loadImage('images/img/img5.png');
+  let img6 = loadImage('images/img/img6.png');
+  let img7 = loadImage('images/img/img7.png');
+
+  immagini_totali = [img0, img1, img2, img3, img4, img5, img6, img7];
+
 }
 
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(900, 200);
 
   noStroke();
-  fill(172, 20, 90);
-  rect(0, 0, width / 2, height);
+  fill(241, 161, 70);
+  rect(0, 0, width / 3, height);
 
-  fill(0, 40, 85);
-  rect(width / 2, 0, width / 2, height);
+  fill(90, 160, 204);
+  rect(width / 3, 0, width / 3, height);
+
+  fill(60, 130, 68);
+  rect(width / (3 / 2), 0, width / (3 / 2), height);
 
   button1 = createButton('Modalità disegna');
   button1.mousePressed(mod_disegna);
-  button1.position((width * (1 / 4)) - centra, height / 8);
+  button1.position((width / 6) - centra, height / 2);
 
   button2 = createButton('Modalità immagine');
   button2.mousePressed(mod_immagine);
-  button2.position((width * (3 / 4)) - centra, height / 8);
+  button2.position((width / 2) - centra, height / 2);
+
+  button5 = createButton('Modalità genitori');
+  button5.mousePressed(mod_genitori);
+  button5.position((width * (5 / 6)) - centra, height / 2);
 
   button3 = createButton('Genera Racconto');
   button4 = createButton('Leggi Racconto');
 
   lunghezza_racconto = createDiv('Lunghezza racconto');
-  lunghezza_racconto.position(xbottoni, 80);
+  lunghezza_racconto.position(xbottoni, 280);
 
   temperatura_racconto = createDiv('Temperatura');
-  temperatura_racconto.position(xbottoni, 140);
+  temperatura_racconto.position(xbottoni, 350);
 
 
   slider1 = createSlider(200, 3000, 800);
   slider2 = createSlider(0, 1, 0.5, 0.1);
 
-  slider1.position(xbottoni, 100);
-  slider2.position(xbottoni, 160);
-  button3.position(xbottoni, 260);
-  button4.position(xbottoni, 320);
+  slider1.position(xbottoni, 310);
+  slider2.position(xbottoni, 370);
+  button3.position(xbottoni, 470);
+  button4.position(xbottoni, 530);
 
   sel1 = createSelect();
-  sel1.position(xbottoni, 240);
+  sel1.position(xbottoni, 450);
   sel1.option('Seleziona un autore..');
   sel1.option('Woolf');
   sel1.option('Baldwin');
@@ -107,7 +136,7 @@ function setup() {
   sel1.changed(mySelectEvent);
 
   sel2 = createSelect();
-  sel2.position(xbottoni, 300);
+  sel2.position(xbottoni, 510);
   sel2.option('Seleziona un lettore..');
   sel2.option('Moira');
   sel2.option('Diego');
@@ -119,6 +148,9 @@ function setup() {
   charRNN = ml5.charRNN('./models/baldwin/', modelReadyBaldwin);
 
   speech = new p5.Speech();
+
+  console.log("Ecco il prototipo di Stories.jpg. Qui sono testate le diverse modalità di utilizzo del sistema. Per saperne di più visita la pagina github di riferimento con la spiegazine del progetto al seguente link: https://github.com/peterbaru/archive/tree/master/peterbaru/MakingVisible");
+  console.log("Seleziona una modalità per generare una storia!");
 }
 
 function mySelectEvent() {
@@ -198,28 +230,52 @@ function objectPronto() {
 }
 
 function mod_disegna() {
+  console.log("Hai selezionato la modalità disegna");
+  seleziona = 2;
   canvas = createCanvas(280, 280);
-  canvas.position(100, 100);
+  canvas.position(100, 150);
   background(220);
   canvas.mouseReleased(classifyCanvas);
-  seleziona = 2;
 }
 
 function mod_immagine() {
-
+  console.log("Hai selezionato la modalità immagine");
   seleziona = 3;
-
-  var cnv1 = createCanvas(400, 400);
-  cnv1.position(340, 100);
+  var cnv1 = createCanvas(400, 280);
+  cnv1.position(340, 150);
   background(220);
 
   sel3 = createSelect();
-  sel3.position(xbottoni, 360);
+  sel3.position(xbottoni, 570);
   sel3.option('Seleziona un disegno..');
   sel3.option('Barca');
   sel3.option('Disegno Famiglia');
   sel3.option('Cane');
   sel3.changed(mySelectEvent3);
+}
+
+function mod_genitori() {
+  console.log("Hai selezionato la modalità genitori");
+  seleziona = 4;
+  cnv2 = createCanvas(canvasgenX, canvasgenY);
+  cnv2.position(100, 150);
+  background(250);
+
+  rnd1 = random(immagini_totali);
+  rnd2 = random(immagini_totali);
+  rnd3 = random(immagini_totali);
+  rnd4 = random(immagini_totali);
+
+  if (rnd1 != rnd2 && rnd1 != rnd3 && rnd1 != rnd4 && rnd2 != rnd3 && rnd2 != rnd4 && rnd3 != rnd4) {
+    // console.log('primo valore: ' + rnd1 + ', secondo valore: ' + rnd2 + ', terzo valore: ' + rnd3 + ', quarto valore: ' + rnd4);
+
+    immagini_genitori = [rnd1, rnd2, rnd3, rnd4];
+
+    immagine_genitore();
+
+  } else {
+    mod_genitori();
+  }
 }
 
 function mySelectEvent3() {
@@ -238,14 +294,6 @@ function mySelectEvent3() {
   }
 }
 
-function immagine_caricata() {
-
-  image(img, 0, 0);
-  console.log('immagine caricata!')
-
-  classifier1.classify(img, gotResult);
-}
-
 function draw() {
 
   if (seleziona == 2) {
@@ -255,9 +303,28 @@ function draw() {
     if (mouseIsPressed) {
       line(pmouseX, pmouseY, mouseX, mouseY);
     }
+
   } else if (seleziona == 3) {
 
+  } else if (seleziona == 4) {
+    image(rnd1, 0, 0, canvasgenX / 2, canvasgenY / 2);
+    image(rnd2, canvasgenX / 2, 0, canvasgenX / 2, canvasgenY / 2);
+    image(rnd3, 0, canvasgenY / 2, canvasgenX / 2, canvasgenY / 2);
+    image(rnd4, canvasgenX / 2, canvasgenY / 2, canvasgenX / 2, canvasgenY / 2);
   }
+}
+
+function immagine_caricata() {
+
+  image(img, 0, 0, 400, 280);
+  console.log('immagine caricata!')
+
+  classifier1.classify(img, gotResult);
+}
+
+function immagine_genitore() {
+
+  classifier1.classify(rnd1, gotResult);
 }
 
 function classifyCanvas() {
@@ -271,17 +338,20 @@ function gotResult(error, results) {
   }
 
   let risultati = results[0].label;
-  console.log(risultati);
+  //console.log(risultati);
 
   inp = createInput(risultati);
   inp.position(xbottoni, 200);
   inp.input(eventoInput);
   eventoInput();
+
+
 }
 
 function eventoInput() {
   textInput = inp.value();
-  console.log(textInput);
+  //  console.log(textInput);
+  console.log("vedo un " + textInput + "! Clicca su 'Genera racconto' per sapere la sua storia.");
   button3.mousePressed(generate);
 }
 
@@ -327,22 +397,19 @@ function generate() {
 
         let num_punti = (frasi.length) - 1;
 
-        let finale = frasi.slice(0, num_punti);
+        //    let storia = frasi.slice(0, num_punti);
 
-        // for (i=0; i<=num_punti; i++){
-        // let def = frasi[i] + ". ";
-        // console.log(def);
-        // let unisci_frasi = def + def;
-        // console.log(unisci_frasi);
-        // }
+        let storia = "";
+        for (i = 0; i < num_punti; i++) {
+          storia += frasi[i] + ". "
+        }
 
-        console.log('Ora puoi leggere il tuo racconto!');
-        let testo_finito = 'Once upon a time a ' + txt + finale + ".";
-        //  console.log(result.sample);
+        console.log("Ora puoi leggere il tuo racconto! se lo vuoi ascoltare seleziona un lettore e clicca su 'Leggi racconto'");
+        let testo_finito = 'Once upon a time a ' + txt + storia;
 
         //  console.log(testo_finito);
         var stampa = createP(testo_finito);
-        stampa.position(40, 380);
+        stampa.position(40, 580);
         button4.mousePressed(leggi);
 
         function leggi() {
