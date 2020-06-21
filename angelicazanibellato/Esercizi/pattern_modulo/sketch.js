62d9a07ece744a55a5bf68a2c45153e17e97e703
angelica zanibellato <62056718+angelicazanibellato@users.noreply.github.com>
2020-06-21 13:58:42 +0200

    Update sketch.js

--- angelicazanibellato/Esercizi/pattern_modulo/sketch.js
+++ angelicazanibellato/Esercizi/pattern_modulo/sketch.js
@@ -1,40 +1,46 @@
-  
-// -
-// Camminatori che generano mondi 0.6 by Angelica [modulo, push/pop]
-// 2020 © Angelica, Daniele @Fupete and the course DSII2020 at DESIGN.unirsm 
-// github.com/dsii-2020-unirsm — github.com/fupete
-// Educational purposes, MIT License, 2020, San Marino
+  // -
+  // Camminatori che generano mondi 0.6 by Angelica [modulo, push/pop]
+  // 2020 © Angelica, Daniele @Fupete and the course DSII2020 at DESIGN.unirsm 
+  // github.com/dsii-2020-unirsm — github.com/fupete
+  // Educational purposes, MIT License, 2020, San Marino
 
- //Help:
-// [mouse] change pattern's color
+  //Help:
+  // [mouse] change pattern's color
 
-let modulo = 35;
+  let dim = 25;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
-  rectMode(CENTER);
-  frameRate(50);
-}
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+    rectMode(CENTER);
+    frameRate(10);
+  }
 
 
-function draw() {
- for (x=0; x<(width*2); x+= modulo+35){
-    for (y=0; y<(height*2); y+= modulo+35){
-      
-      
- push();
-      this.r = map(mouseX, 0, width, 0, 255)
-      this.g = map(mouseY, 0, height, 0, 255)
-      this.b = random(255)
-      stroke(this.r, this.g, this.b);
-      noFill();
-      circle(x, y, frameCount%(modulo*2), modulo);
-      rect(x+modulo, y+modulo, frameCount%(modulo*2),frameCount%(modulo*2),2);
+  function draw() {
+    background(50);
+    for (let x = 0; x < 50; x++) {
+      for (let y = 0; y < 50; y++) {
 
-      pop();
+        if (x % 8 == 0 || y % 8 == 0) {
+          noStroke();
+          this.r = map(mouseX, 0, width, 0, 255)
+          this.g = map(mouseY, 0, height, 0, 255)
+          this.b = random(255)
+          fill(this.r, this.g, this.b);
+          //stroke(this.r, this.g, this.b);   
+          ellipse(x * dim, y * dim, frameCount % 50)
+        } else {
+          noStroke();
+          this.r = map(mouseX, 0, width, 0, 255);
+          this.g = map(mouseY, 0, height, 0, 255);
+          this.b = random(255);
+          //strokeWeight(2);
+          fill(this.r, this.g, this.b);
 
-           
+          rect(x * 25, y * 25, frameCount % 25, frameCount % 25, 2)
+        }
+
+
+      }
     }
- }
-}
+  }
